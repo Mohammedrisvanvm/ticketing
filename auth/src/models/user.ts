09f297dfff655e7a45ed11f1ddf9410ae14dc15f
Mohammedrisvanvm <risvanrishu0000@gmail.com>
2025-04-17 21:32:59 +0530

    implimented hashing for password

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -1,4 +1,5 @@
 import mongoose, { InferSchemaType } from "mongoose";
+import { Password } from "../service/password";
 
 interface UserAttrs {
   email: string;
@@ -30,6 +31,14 @@ const userSchema = new mongoose.Schema(
   },
   { timestamps: true }
 );
+
+userSchema.pre("save", async function (done) {
+  if (this.isModified("password")) {
+    const hashedPassword = await Password.toHash(this.get("password"));
+    this.set("password", hashedPassword);
+  }
+  done();
+});
 userSchema.statics.build = (attrs: UserAttrs) => {
   return new User(attrs);
 };
