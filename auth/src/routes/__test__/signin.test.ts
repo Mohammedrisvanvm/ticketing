4b3c9711b9f0b9a0508a7b4ab164073885bf565b
Mohammedrisvanvm <risvanrishu0000@gmail.com>
2025-04-23 19:55:13 +0530

    test: add signin and signout tests for user authentication

--- auth/src/routes/__test__/signin.test.ts
+++ auth/src/routes/__test__/signin.test.ts
@@ -1,19 +1,9 @@
 import request from "supertest";
 import { app } from "../../app";
 
-it("returns a 201 on successful signup", async () => {
-  await request(app)
-    .post("/api/users/signup")
-    .send({
-      email: "risvan@gmail.com",
-      password: "123456",
-    })
-    .expect(201);
-});
-
 it("returns a 400 with an invalid email", async () => {
   return request(app)
-    .post("/api/users/signup")
+    .post("/api/users/signin")
     .send({
       email: "risvangmail.com",
       password: "123456",
@@ -22,7 +12,7 @@ it("returns a 400 with an invalid email", async () => {
 });
 it("returns a 400 with an invalid password", async () => {
   return request(app)
-    .post("/api/users/signup")
+    .post("/api/users/signin")
     .send({
       email: "risvan@gmail.com",
       password: "123",
@@ -30,33 +20,44 @@ it("returns a 400 with an invalid password", async () => {
     .expect(400);
 });
 it("returns a 400 with missing email and password", async () => {
-  await request(app).post("/api/users/signup").send({}).expect(400);
+  await request(app).post("/api/users/signin").send({}).expect(400);
 });
 
-it("disallows duplicate emails", async () => {
+it("email not found", async () => {
   await request(app)
-    .post("/api/users/signup")
+    .post("/api/users/signin")
     .send({
-      email: "risvan@gmail.com",
-      password: "123456",
-    })
-    .expect(201);
-  await request(app)
-    .post("/api/users/signup")
-    .send({
-      email: "risvan@gmail.com",
+      email: "risva@gmail.com",
       password: "123456",
     })
     .expect(400);
 });
 
-it("sets a cookie after successful signup", async () => {
-  const response = await request(app)
+it("password not found", async () => {
+  await request(app)
+    .post("/api/users/signin")
+    .send({
+      email: "risvan@gmail.com",
+      password: "12345",
+    })
+    .expect(400);
+});
+
+it("cookie after successful signin", async () => {
+ await request(app)
     .post("/api/users/signup")
     .send({
       email: "risvan@gmail.com",
       password: "123456",
     })
     .expect(201);
+
+  const response = await request(app)
+    .post("/api/users/signin")
+    .send({
+      email: "risvan@gmail.com",
+      password: "123456",
+    })
+    .expect(200);
   expect(response.get("Set-Cookie")).toBeDefined();
 });
