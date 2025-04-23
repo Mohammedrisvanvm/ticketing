b04fe5c962182210d3ab85ded4ee71c5bc38af50
Mohammedrisvanvm <risvanrishu0000@gmail.com>
2025-04-23 19:55:21 +0530

    test: add signout test to verify cookie removal

---
+++ auth/src/routes/__test__/signout.test.ts
@@ -0,0 +1,26 @@
+import request from "supertest";
+import { app } from "../../app";
+
+it("coockies remove after signout", async () => {
+  await request(app)
+    .post("/api/users/signup")
+    .send({
+      email: "risvan@gmail.com",
+      password: "12345",
+    })
+    .expect(201);
+
+  const response = await request(app)
+    .post("/api/users/signout")
+    .send({})
+    .expect(200);
+
+  const cookie = response.get("Set-Cookie");
+  if (!cookie) {
+    throw new Error("Expected cookie but got undefined.");
+  }
+
+  expect(cookie[0]).toEqual(
+    "session=; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT; httponly"
+  );
+});
