5d4e28e6d4279161a08afb2055bb9a4f1072d75f
Mohammedrisvanvm <risvanrishu0000@gmail.com>
2025-04-24 21:54:25 +0530

    feat(client): refactor LandingPage component to fetch current user data

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,8 +1,31 @@
-export default () => {
-  return (
-    <div>
-      <h1 className="text-primary">Ticketing</h1>
-      <p>Ticketing appxxxxxxxxxxxxxxxxication</p>
-    </div>
-  );
-}
+import axios from "axios";
+
+const LandingPage = ({ currentUser }) => {
+  console.log(currentUser);
+  // axios.get("/api/users/currentuser").catch((err) => {
+  //   console.log(err.message);
+  // });
+
+  return <h1>Landing Page</h1>;
+};
+
+LandingPage.getInitialProps = async ({ req }) => {
+  if (typeof window === "undefined") {
+    // We are on the server
+    const { data } = await axios.get(
+      "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser",
+      {
+        headers: req.headers,
+      }
+    );
+
+    return data;
+  } else {
+    // We are on the browser
+    const { data } = await axios.get("/api/users/currentuser");
+    return data;
+  }
+  return {};
+};
+
+export default LandingPage;
