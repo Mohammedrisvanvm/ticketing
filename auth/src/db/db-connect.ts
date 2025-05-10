f239c8086db2f7211e41778a4e6fa3f5aa09a77b
Mohammedrisvanvm <risvanrishu0000@gmail.com>
2025-05-10 20:06:09 +0530

    fix: update db connection to use environment variables for MONGO_URI and JWT_KEY

--- auth/src/db/db-connect.ts
+++ auth/src/db/db-connect.ts
@@ -4,8 +4,11 @@ const connectDB = async () => {
   if (!process.env.JWT_KEY) {
     throw new Error("must be defined");
   }
+  if (!process.env.MONGO_URI) {
+    throw new Error("MONGO_URI must be defined");
+  }
   try {
-    await mongoose.connect("mongodb://auth-mongo-srv:27017/auth", {});
+    await mongoose.connect(process.env.MONGO_URI, {});
     console.log("MongoDB connected successfully!");
   } catch (error) {
     console.error(error);
