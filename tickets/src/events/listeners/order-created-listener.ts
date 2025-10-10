3cdd27f02c1970f08e2363a6ef55b658d1c79b5d
Mohammedrisvanvm <risvanrishu0000@gmail.com>
2025-10-10 19:32:33 +0530

    feat: enhance OrderCreatedListener to associate tickets with orders and update Ticket model to include orderId field

--- tickets/src/events/listeners/order-created-listener.ts
+++ tickets/src/events/listeners/order-created-listener.ts
@@ -1,10 +1,18 @@
 import { Listener, OrderCreatedEvent, Subjects } from "@risvantickets/common";
 import { Message } from "node-nats-streaming";
+import { Ticket } from "../../models/ticket";
 
 export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
   subject: Subjects.OrderCreated = Subjects.OrderCreated;
   queueGroupName = "tickets-service";
   async onMessage(data: OrderCreatedEvent["data"], msg: Message) {
+    const ticket = await Ticket.findById(data.ticket.id);
+    if (!ticket) {
+      throw new Error("Ticket not found");
+    }
+    ticket.set({ orderId: data.id });
+    await ticket.save();
+
     console.log("Event data!", data);
     msg.ack();
   }
