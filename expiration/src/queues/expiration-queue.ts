332c6bfbb27818b19d5e8849cfbdd7192aef0b89
Mohammedrisvanvm <risvanrishu0000@gmail.com>
2025-10-12 19:53:13 +0530

    feat: add expiration queue for processing order expiration jobs

---
+++ expiration/src/queues/expiration-queue.ts
@@ -0,0 +1,16 @@
+import Queue from "bull";
+
+interface Payload {
+  orderId: string;
+}
+const expirationQueue = new Queue<Payload>("order:expiration", {
+  redis: {
+    host: process.env.REDIS_HOST,
+  },
+});
+
+expirationQueue.process(async (job) => {
+  console.log("I want to expire order", job.data.orderId);
+});
+
+export { expirationQueue };
