094a0c9f4f3c4590418e2e4137b8b23511217b89
Mohammedrisvanvm <risvanrishu0000@gmail.com>
2025-10-12 20:25:26 +0530

    feat: implement expiration processing by publishing expiration complete events

--- expiration/src/queues/expiration-queue.ts
+++ expiration/src/queues/expiration-queue.ts
@@ -1,4 +1,6 @@
 import Queue from "bull";
+import { ExpirationCompletePublisher } from "../events/publishers/expiration-complete-publisher";
+import { natsWrapper } from "../nats/nats-wrapper";
 
 interface Payload {
   orderId: string;
@@ -10,7 +12,9 @@ const expirationQueue = new Queue<Payload>("order:expiration", {
 });
 
 expirationQueue.process(async (job) => {
-  console.log("I want to expire order", job.data.orderId);
+  new ExpirationCompletePublisher(natsWrapper.client).publish({
+    orderId: job.data.orderId,
+  });
 });
 
 export { expirationQueue };
