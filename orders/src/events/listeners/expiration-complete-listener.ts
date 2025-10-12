c585075f2ad5e8524534d51078ec3f5a90f3157e
Mohammedrisvanvm <risvanrishu0000@gmail.com>
2025-10-12 20:38:50 +0530

    feat: update @risvantickets/common dependency to version 1.0.11 and add ExpirationCompleteListener for handling expiration events

---
+++ orders/src/events/listeners/expiration-complete-listener.ts
@@ -0,0 +1,31 @@
+import {
+  ExpirationCompleteEvent,
+  Listener,
+  orderStatus,
+  Subjects,
+} from "@risvantickets/common";
+import { Message } from "node-nats-streaming";
+import { Order } from "../../models/order";
+import { OrderCancelledPublisher } from "../publishers/order-cancelled-publisher";
+
+export class ExpirationCompleteListener extends Listener<ExpirationCompleteEvent> {
+  subject: Subjects.ExpirationComplete = Subjects.ExpirationComplete;
+  queueGroupName = "orders-service";
+  async onMessage(data: ExpirationCompleteEvent["data"], msg: Message) {
+    const order = await Order.findById(data.orderId);
+    if (!order) {
+      throw new Error("Order not found");
+    }
+    
+    order.status = orderStatus.Cancelled;
+    await order.save();
+
+    new OrderCancelledPublisher(this.client).publish({
+      id: order.id,
+      version: order.version,
+        ticket: { id: order.ticket.id },
+    });
+    
+    msg.ack();
+  }
+}
