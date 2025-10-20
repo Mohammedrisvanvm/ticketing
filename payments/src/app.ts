b1ed45017f4a643dd6793729d74877bbbad7dc9d
Mohammedrisvanvm <risvanrishu0000@gmail.com>
2025-10-21 01:47:02 +0530

    feat: remove ticket routes from app and add MongoDB connection utility

--- payments/src/app.ts
+++ payments/src/app.ts
@@ -6,10 +6,7 @@ import {
 import { json } from "body-parser";
 import cookieSession from "cookie-session";
 import express, { NextFunction, Request, Response } from "express";
-import { createTicketRouter } from "./routes/new";
-import { showTicketRouter } from "./routes/show";
-import { indexTicketRouter } from "./routes";
-import { updateTicketRouter } from "./routes/update";
+
 
 const app = express();
 
@@ -19,11 +16,6 @@ app.use(
   cookieSession({ signed: false, secure: process.env.NODE_ENV !== "test" })
 ); // Set secure to true in production
 app.use(sessionUser);
-
-app.use(createTicketRouter);
-app.use(showTicketRouter);
-app.use(indexTicketRouter);
-app.use(updateTicketRouter);
 
 app.use(async (req: Request, res: Response, next: NextFunction) => {
   next(new NotFoundError());
