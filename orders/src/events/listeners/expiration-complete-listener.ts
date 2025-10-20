f9256c6f92cf13f02c9a39814af2ea0d46cc62d9
Mohammedrisvanvm <risvanrishu0000@gmail.com>
2025-10-21 03:01:27 +0530

    fix(listener): populate ticket data when retrieving order in ExpirationCompleteListener

--- orders/src/events/listeners/expiration-complete-listener.ts
+++ orders/src/events/listeners/expiration-complete-listener.ts
@@ -12,7 +12,7 @@ export class ExpirationCompleteListener extends Listener<ExpirationCompleteEvent
   subject: Subjects.ExpirationComplete = Subjects.ExpirationComplete;
   queueGroupName = "orders-service";
   async onMessage(data: ExpirationCompleteEvent["data"], msg: Message) {
-    const order = await Order.findById(data.orderId);
+    const order = await Order.findById(data.orderId).populate("ticket");
     if (!order) {
       throw new Error("Order not found");
     }
@@ -23,7 +23,7 @@ export class ExpirationCompleteListener extends Listener<ExpirationCompleteEvent
     new OrderCancelledPublisher(this.client).publish({
       id: order.id,
       version: order.version,
-        ticket: { id: order.ticket.id },
+      ticket: { id: order.ticket.id },
     });
     
     msg.ack();
