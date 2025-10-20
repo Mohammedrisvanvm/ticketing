234f8b78b59b4e259c20ac35ff390f97b04d342d
Mohammedrisvanvm <risvanrishu0000@gmail.com>
2025-10-21 02:25:29 +0530

    refactor(nats): improve formatting and structure in NATS connection setup

--- payments/src/nats/nats-connect.ts
+++ payments/src/nats/nats-connect.ts
@@ -1,18 +1,23 @@
-
+import { OrderCancelledListener } from "../events/listeners/order-cancelled-listener";
+import { OrderCreatedListener } from "../events/listeners/order-created-listener";
 import { natsWrapper } from "./nats-wrapper";
 
 const connectNats = async () => {
   if (!process.env.NATS_CLUSTER_ID) {
-      throw new Error("NATS_CLUSTER_ID must be defined");
+    throw new Error("NATS_CLUSTER_ID must be defined");
   }
   if (!process.env.NATS_CLIENT_ID) {
-      throw new Error("NATS_CLIENT_ID must be defined");
+    throw new Error("NATS_CLIENT_ID must be defined");
   }
   if (!process.env.NATS_URL) {
-      throw new Error("NATS_URL must be defined");
+    throw new Error("NATS_URL must be defined");
   }
   try {
-    await natsWrapper.connect(process.env.NATS_CLUSTER_ID, process.env.NATS_CLIENT_ID, process.env.NATS_URL);
+    await natsWrapper.connect(
+      process.env.NATS_CLUSTER_ID,
+      process.env.NATS_CLIENT_ID,
+      process.env.NATS_URL
+    );
     natsWrapper.client.on("close", () => {
       console.log("nats connection closed!");
       process.exit();
@@ -20,6 +25,9 @@ const connectNats = async () => {
     process.on("SIGINT", () => natsWrapper.client.close());
     process.on("SIGTERM", () => natsWrapper.client.close());
 
+    new OrderCreatedListener(natsWrapper.client).listen();
+    new OrderCancelledListener(natsWrapper.client).listen();
+    
     console.log("NATS connected successfully!");
   } catch (error) {
     console.error(error);
