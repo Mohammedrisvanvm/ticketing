9a3cc572dee3d93168a50ce3bab4ab280f6c235f
Mohammedrisvanvm <risvanrishu0000@gmail.com>
2025-10-21 01:39:46 +0530

    feat(payments): initialize payments service with Express, NATS, and MongoDB
    - Added package.json with dependencies and scripts for the payments service.
    - Created app.ts to set up Express server with routes and error handling.
    - Implemented index.ts to connect to NATS and MongoDB, and start the server.
    - Added NATS wrapper and connection logic for event-driven architecture.
    - Included Jest setup for testing with MongoDB in-memory server.
    - Configured TypeScript with tsconfig.json for strict type checking.
    - Updated skaffold.yaml to include payments service for deployment.

---
+++ payments/src/test/setup.ts
@@ -0,0 +1,56 @@
+import Jwt from "jsonwebtoken";
+import { MongoMemoryServer } from "mongodb-memory-server";
+import mongoose from "mongoose";
+
+
+declare global {
+  var signin: () => string[];
+}
+
+let mongo: any;
+jest.mock("../nats/nats-wrapper");
+beforeAll(async () => {
+  process.env.JWT_KEY = "test";
+  mongo = await MongoMemoryServer.create();
+  const mongoUri = mongo.getUri();
+
+  await mongoose.connect(mongoUri, {});
+});
+
+beforeEach(async () => {
+  if (mongoose.connection.db) {
+    const collections = await mongoose.connection.db.collections();
+
+    for (let collection of collections) {
+      await collection.deleteMany({});
+    }
+  }
+});
+
+afterAll(async () => {
+  if (mongo) {
+    await mongo.stop();
+  }
+  await mongoose.connection.close();
+});
+
+global.signin = () => {
+  // Build a jwt payload { id, email }
+  const payload = {
+    id: new mongoose.Types.ObjectId().toHexString(),
+    email: "risvantest@gmail.com",
+  };
+
+  // Create the JWT!
+
+  const token = Jwt.sign(payload, process.env.JWT_KEY!);
+  // Take the JWT and store it on the session object
+  const session = { jwt: token };
+  // turn the session as json
+  const sessionJSON = JSON.stringify(session);
+  // turn the json into base64
+  const base64 = Buffer.from(sessionJSON).toString("base64");
+  // return a string that is the cookie with the encoded data
+
+  return [`session=${base64}`];
+};
