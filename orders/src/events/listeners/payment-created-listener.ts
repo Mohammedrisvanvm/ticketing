48b71ab89e5cbadd2878b9e7f2a69108dee4a0dd
Mohammedrisvanvm <risvanrishu0000@gmail.com>
2025-10-24 04:30:43 +0530

    feat(payments): implement PaymentCreatedListener to handle payment creation events

---
+++ orders/src/events/listeners/payment-created-listener.ts
@@ -0,0 +1,22 @@
+import {
+  Listener,
+  orderStatus,
+  PaymentCreatedEvent,
+  Subjects,
+} from "@risvantickets/common";
+import { Message } from "node-nats-streaming";
+import { Order } from "../../models/order";
+
+export class PaymentCreatedListener extends Listener<PaymentCreatedEvent> {
+  subject: Subjects.PaymentCreated = Subjects.PaymentCreated;
+  queueGroupName = "orders-service";
+  async onMessage(data: PaymentCreatedEvent["data"], msg: Message) {
+    const order = await Order.findById(data.orderId);
+    if (!order) {
+      throw new Error("Order not found");
+    }
+    order.set({ status: orderStatus.Complete });
+    await order.save();
+    msg.ack();
+  }
+}
