e312545c6443109693e1bf7b0d9c544f6441b7ad
Mohammedrisvanvm <risvanrishu0000@gmail.com>
2025-10-24 02:53:59 +0530

    feat(payments): add payment intent creation logic to handle order payments

--- payments/src/routes/new.ts
+++ payments/src/routes/new.ts
@@ -9,6 +9,7 @@ import {
 import { Request, Response, Router } from "express";
 import { body } from "express-validator";
 import { Order } from "../models/order";
+import { stripeClient } from "../config/stripe";
 
 const router = Router();
 
@@ -33,6 +34,13 @@ router.post(
     if (order.status === orderStatus.Cancelled) {
       throw new BadRequestError("Cannot pay for a cancelled order");
     }
+
+    await stripeClient.paymentIntents.create({
+      amount: order.price * 100,
+      currency: "usd",
+      payment_method: token,
+      // confirm: true, // Uncomment if you want to confirm the payment immediately
+    });
 
     res.status(201).send({ success: true });
   }
