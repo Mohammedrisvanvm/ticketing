948571429dd094e6dc327f92a163502d3681e567
Mohammedrisvanvm <risvanrishu0000@gmail.com>
2025-10-24 04:35:08 +0530

    feat(payments): implement to restrict update the cancelled status of already completed records

--- orders/src/events/listeners/expiration-complete-listener.ts
+++ orders/src/events/listeners/expiration-complete-listener.ts
@@ -17,6 +17,10 @@ export class ExpirationCompleteListener extends Listener<ExpirationCompleteEvent
       throw new Error("Order not found");
     }
     
+    if (order.status === orderStatus.Complete) {
+      return msg.ack();
+    }
+    
     order.status = orderStatus.Cancelled;
     await order.save();
 
