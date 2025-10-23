8a0ba7188b5be1abca645996e8bc051a47828ea6
Mohammedrisvanvm <risvanrishu0000@gmail.com>
2025-10-24 01:50:35 +0530

    feat(payments): implement payment creation logic with validation and error handling

--- payments/src/routes/new.ts
+++ payments/src/routes/new.ts
@@ -1,18 +1,40 @@
-import { requireAuth } from "@risvantickets/common";
+import {
+  BadRequestError,
+  NotAuthorizedError,
+  NotFoundError,
+  orderStatus,
+  requireAuth,
+  validateRequest,
+} from "@risvantickets/common";
 import { Request, Response, Router } from "express";
 import { body } from "express-validator";
+import { Order } from "../models/order";
 
 const router = Router();
 
 router.post(
   "/api/payments",
   requireAuth,
-  [
-    body("token").not().notEmpty(),
-    body("orderId").not().notEmpty()
- ],
-  async(req: Request, res: Response) => {
-    res.send("Create a new payment");
+  [body("token").not().notEmpty(), body("orderId").not().notEmpty()],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    const { token, orderId } = req.body;
+
+    const order = await Order.findById(orderId);
+
+    if (!order) {
+      throw new NotFoundError();
+    }
+
+    if (order.userId !== req.currentUser!.id) {
+      throw new NotAuthorizedError();
+    }
+
+    if (order.status === orderStatus.Cancelled) {
+      throw new BadRequestError("Cannot pay for a cancelled order");
+    }
+
+    res.status(201).send({ success: true });
   }
 );
 
