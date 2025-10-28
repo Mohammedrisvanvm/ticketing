f360cbc1c58b4bfc202e567ec184b6ae446f03b4
Mohammedrisvanvm <risvanrishu0000@gmail.com>
2025-10-29 00:49:40 +0530

    feat(frontend): product page and filter added

--- frontend/src/app/products/page.tsx
+++ frontend/src/app/products/page.tsx
@@ -1,7 +1,6 @@
-import Banner from "@/components/Banner";
 import ProductList from "@/components/ProductList";
 
-const Home = async ({
+const productPage = async ({
   searchParams,
 }: {
   searchParams: Promise<{ category: string }>;
@@ -9,10 +8,9 @@ const Home = async ({
   const { category } = await searchParams;
   return (
     <div className="">
-      <Banner />
-      <ProductList category={category} params="homepage" />
+      <ProductList category={category} params="product" />
     </div>
   );
 };
 
-export default Home;
+export default productPage;
--- frontend/src/components/ProductList.tsx
+++ frontend/src/components/ProductList.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import Categories from "./Categories";
 import ProductCard from "./ProductCard";
+import Filter from "./Filter";
 
 export interface DemoProductType {
   id: string;
@@ -41,10 +42,18 @@ const demoProducts: DemoProductType[] = [
   },
 ];
 
-const ProductList = ({ category }: { category: string }) => {
+const ProductList = ({
+  category,
+  params,
+}: {
+  category: string;
+  params: "homepage" | "product";
+}) => {
   return (
     <div className="w-full">
       <Categories />
+      {params === "product" && <Filter />}
+
       <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-12">
         {demoProducts.map((product) => (
           <ProductCard key={product.id} product={product} />
