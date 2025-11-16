334b29e41d3a6ecc4df7d34487d7305de9ce6169
Mohammedrisvanvm <risvanrishu0000@gmail.com>
2025-11-16 18:00:17 +0530

    feat(redis): add readiness and liveness probes to expiration-redis deployment feat(expiration): implement init container to wait for Redis readiness before starting feat(expiration-queue): refactor to use BullMQ for job processing

--- expiration/src/queues/expiration-queue.ts
+++ expiration/src/queues/expiration-queue.ts
@@ -1,24 +1,3 @@
-// import Queue from "bull";
-// import { ExpirationCompletePublisher } from "../events/publishers/expiration-complete-publisher";
-// import { natsWrapper } from "../nats/nats-wrapper";
-
-// interface Payload {
-//   orderId: string;
-// }
-// const expirationQueue = new Queue<Payload>("order:expiration", {
-//   redis: {
-//     host: process.env.REDIS_HOST,
-//   },
-// });
-
-// expirationQueue.process(async (job) => {
-//   new ExpirationCompletePublisher(natsWrapper.client).publish({
-//     orderId: job.data.orderId,
-//   });
-// });
-
-// export { expirationQueue };
-
 import { Queue, Worker } from "bullmq";
 import IORedis from "ioredis";
 import { ExpirationCompletePublisher } from "../events/publishers/expiration-complete-publisher";
@@ -28,24 +7,27 @@ interface Payload {
   orderId: string;
 }
 
-// Redis connection (BullMQ uses ioredis)
 const connection = new IORedis({
   host: process.env.REDIS_HOST,
+  port: 6379,
   maxRetriesPerRequest: null,
+  retryStrategy(times) {
+    return 1000; // retry every second until connected
+  },
 });
 
-// Create the queue
 const expirationQueue = new Queue<Payload>("order-expiration", {
   connection,
 });
 
-// Worker to process jobs
 const worker = new Worker<Payload>(
   "order-expiration",
   async (job) => {
     await new ExpirationCompletePublisher(natsWrapper.client).publish({
       orderId: job.data.orderId,
     });
+
+    console.log("Expiration job completed:", job.data.orderId);
   },
   { connection }
 );
